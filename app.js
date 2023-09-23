782023d235190b04ec52aa8fd62fb323223dc9a2
Pankaj Singh <panku1382002@gmail.com>
2023-09-23 19:02:31 +0530

    s

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', true);
 
 const Blog = require('./models/blog')
 const userRoutes = require('./routes/userRoute');
